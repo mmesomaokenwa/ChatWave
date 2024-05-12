e551e0411ccad71d077061865c571a3c6920c933
Mmesoma Okenwa <godsfavourokenwa12@gmail.com>
2024-05-12 12:36:03 +0100

    Enhanced the ui of post card

--- src/components/shared/PostCard.jsx
+++ src/components/shared/PostCard.jsx
@@ -16,35 +16,42 @@ const PostCard = async ({ post }) => {
   const user = session?.user
 
   return (
-    <Card className='rounded-none md:rounded-xl'>
+    <Card className="rounded-none md:rounded-xl">
       <PostCardWrapper postId={post._id}>
-      <CardHeader className="flex-row gap-4 px-4 lg:p-4">
-        <User
-          avatarProps={{
-            src:
-              post?.creator?.profileImage || "/assets/profile-placeholder.svg",
-            size: "md",
-          }}
-          name={post?.creator?.name}
-          description={
-            <>
-              <span>{timePast(post.createdAt)}</span>
-              {post.location && <span className='mx-1'>&#x2022;</span>}
-              {post.location && <span>{post.location}</span>}
-            </>
-          }
-          classNames={{
-            name: "font-medium"
-          }}
-        />
-        {user?.id === post.creator._id.toString() && (
-          <PostTopControls post={post} />
-        )}
-      </CardHeader>
+        <CardHeader className="flex-row gap-4 px-4 lg:p-4">
+          <Link href={`/profile/${post.creator._id}`}>
+            <User
+              avatarProps={{
+                src:
+                  post?.creator?.profileImage ||
+                  "/assets/profile-placeholder.svg",
+                size: "md",
+              }}
+              name={post?.creator?.name}
+              description={
+                <>
+                  <span>{timePast(post.createdAt)}</span>
+                  {post.location && <span className="mx-1">&#x2022;</span>}
+                  {post.location && <span>{post.location}</span>}
+                </>
+              }
+              classNames={{
+                name: "font-medium",
+              }}
+            />
+          </Link>
+          {user?.id === post.creator._id.toString() && (
+            <PostTopControls post={post} />
+          )}
+        </CardHeader>
       </PostCardWrapper>
       <CardBody className="px-4 flex flex-col gap-1">
         <PostCaptionAndTags post={post} />
-        <CardCarousel postMedia={post.media} radius={'lg'} size={'max-h-[300px]'} />
+        <CardCarousel
+          postMedia={post.media}
+          radius={"lg"}
+          size={"max-h-[300px]"}
+        />
       </CardBody>
       <CardFooter className="p-4 pt-0">
         <PostControls post={post} />
@@ -54,33 +61,3 @@ const PostCard = async ({ post }) => {
 }
 
 export default PostCard
-
-export const PostTitle = ({ post }) => (
-  <Link href={`/profile/${post.creator._id}`}>
-    <CardTitle className="font-bold text-sm lg:text-[16px]">
-      {post.creator.username}
-    </CardTitle>
-  </Link>
-);
-
-export const PostDescription = ({ post, isFullDate }) => (
-  <CardDescription className="flex gap-2 text-xs lg:text-sm font-semibold items-center">
-    <span>
-      {isFullDate ? formatDateString(post.createdAt) : timePast(post.createdAt)}
-    </span>
-    {post.location && <span>&#x2022;</span>}
-    {post.location && <span>{post.location}</span>}
-  </CardDescription>
-);
-
-export const PostProfileImage = ({ post }) => (
-  <Link href={`/profile/${post.creator._id}`}>
-    <Image
-      className="size-12 rounded-full"
-      src={post.creator.profileImage || "/assets/profile-placeholder.svg"}
-      alt={post.creator.username}
-      width={48}
-      height={48}
-    />
-  </Link>
-);
