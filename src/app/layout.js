51c43b47d08c4bce6393a589628dd9df0a6ad20b
Mmesoma Okenwa <godsfavourokenwa12@gmail.com>
2024-05-13 08:07:50 +0100

    Changed all page layouts

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,7 +32,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body className={poppins.className} style={{overflow: 'auto'}}>
         <UIProvider>
           <AuthProvider>
             <SocketProvider>
